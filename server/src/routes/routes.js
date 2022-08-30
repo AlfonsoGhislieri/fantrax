07f9968ea13f838acd4f2ae8b01b389119fe29e9
Alfonso Ghislieri <alfonso.ghis@hotmail.fr>
2022-08-30 11:01:54 +0200

    created addPlayer route

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,26 @@
 import express from "express";
-import PlayerModel from "../models/player.model.js";
+import playerModel from "../models/player.model.js";
 
 const routes = express();
 
-routes.get("/test", (req, res) => {
+routes.get("/test", async (req, res) => {
   return res.json({ message: "Hello from server!" });
 });
 
+routes.post("/addPlayer", async (req, res) => {
+  const data = req.body;
+  const name = data.name;
+  const position = data.position;
+  const value = parseInt(data.value);
+  const owner = data.owner;
+
+  const test = new playerModel({
+    name: name,
+    position: position,
+    value: value,
+    owner: owner,
+  });
+
+  res.json(test);
+});
 export default routes;
