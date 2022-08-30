46927f223cf994c6afbd62c5e02a2660331751e6
Alfonso Ghislieri <alfonso.ghis@hotmail.fr>
2022-08-30 10:35:43 +0200

    changed port for server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
