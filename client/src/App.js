72035179d0498e82a2904fbbb813fa65e462d212
Alfonso Ghislieri <alfonso.ghis@hotmail.fr>
2022-08-30 11:13:11 +0200

    setup router and file structure for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+// Pages
+import SellPlayersPage from "./pages/SellPlayersPage";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/sellPlayers" element={<SellPlayersPage />} />
+          <Route path="*" element={<SellPlayersPage />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
